e32663ce6a400bd317afee668bfa1f0cacc5c2f3
Thang Nguyen <thangnd@stageit.app>
2022-04-28 16:17:06 +0700

    fix

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ import express from 'express'
 import Router from './src/routes/music.js'
 import pkg from "body-parser";
 import cors from './src/cors.js'
-import * as http from "http";
 
 const {  json } = pkg
 const app = express()
-const port = 3000
-
-let addr = http.Server.address();
-let bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-
 app.use(json())
 app.use(cors())
 app.use('/music', Router)
 
-app.listen(bind, () => {
-  console.log(`listening on port ${bind}`)
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
