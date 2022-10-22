47068953cfd2f3c3116f4f747dec091ad1f77633
thangnd <thangnd@stageit.app>
2022-10-22 22:25:55 +0700

    update axios

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,9 @@ import { db } from '../configs/firebase.config.js'
 import express from 'express'
 import { uuid } from 'uuidv4';
 
+import axios from 'axios';
+
+
 const router = express.Router()
 export default router;
 
@@ -29,6 +32,17 @@ router.get('/read', async (req, res) => {
       res.send(error);
     }
     
+  });
+
+  router.get('/list-film', async (req, res, next) => {
+      const nameSearch = req.query.key || "";
+    axios({
+      method: 'get',
+      url: `https://ophim.cc/_next/data/2uibqhufwNeudgKQPPGsA/tim-kiem.json?keyword=${nameSearch}`,
+    })
+      .then(function (response) {
+        res.json(response.data)
+      });  
   });
 
   router.post('/create', async (req, res) => {
@@ -60,11 +74,29 @@ router.get('/read', async (req, res) => {
     }
   });
 
-  router.delete('/delete/:id', async (req, res) => {
+  router.delete('/delete/:id', async (req, res, next) => {
     try {
       const response = await db.collection("users").doc(req.params.id).delete();
       res.send(response);
     } catch(error) {
       res.send(error);
     }
+  })
+
+  router.post('/login', async (req, res, next) => {
+    try {
+      const email = req.body.email || "";
+      const password = req.body.password || "";
+
+      if (email === "" || password === "") {
+          throw new Error("Missing field")
+      }
+      
+      const userRef = db.collection("users")
+      const response = await userRef.where('name', '==', `${email}`);
+      res.json(response.get().data());
+      
+    } catch(error) {
+      next(error)
+    }  
   })
