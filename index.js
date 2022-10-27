3e522661f0d66d68e2cbfdf54fe00f0b146020e9
thangnd <thangnd@stageit.app>
2022-10-27 17:54:51 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { json } = pkg
 const app = express()
 app.use(json())
 app.use(cors())
-app.use('/music', Router)
+app.use('/api/v1', Router)
 
 app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,10 @@
 import { db } from '../configs/firebase.config.js'
 import express from 'express'
 import { uuid } from 'uuidv4';
-
+import * as bcrypt from "bcrypt"
+import { collection, query, where, getDocs } from "firebase/firestore";
+import jwt from "jsonwebtoken";
+import {authorization} from "../middleware/authorization.js"
 
 const router = express.Router()
 export default router;
@@ -16,7 +19,7 @@ router.get('/read/:id', async (req, res) => {
     }
   });
 
-router.get('/read', async (req, res) => {
+router.get('/read', authorization, async (req, res) => {
     try {
         db.collection('users').onSnapshot((snapshot) => {
             const data = snapshot.docs.map(doc => ({
@@ -34,16 +37,24 @@ router.get('/read', async (req, res) => {
 
   router.post('/create', async (req, res) => {
     try {
-      console.log(req.body);
-      const userJson = {
-        email: req.body.email,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
-      };
-      const usersDb = db.collection('users'); 
-      await usersDb.doc(uuid()).set(userJson);
-      res.json({message: "created success"});
-    } catch(error) {
+      const saltRound = 10;
+
+      bcrypt.genSalt(saltRound, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, async (err, hash_password) => {
+          req.body.password = hash_password
+          const userJson = {
+            email: req.body.email,
+            password: req.body.password,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            address: req.body.address
+          };
+          const usersDb = db.collection('users');
+          await usersDb.doc(uuid()).set(userJson);
+          res.json({ message: "created success" });
+        })
+      })
+    } catch (error) {
       res.json(error);
     }
   });
@@ -72,18 +83,41 @@ router.get('/read', async (req, res) => {
 
   router.post('/login', async (req, res, next) => {
     try {
+      const datas = [];
       const email = req.body.email || "";
       const password = req.body.password || "";
 
       if (email === "" || password === "") {
-          throw new Error("Missing field")
+        throw new Error("Missing field")
       }
-      
-      const userRef = db.collection("users")
-      const response = await userRef.where('name', '==', `${email}`);
-      res.json(response.get().data());
-      
-    } catch(error) {
+
+      const q = query(collection(db, "users"), where("email", "==", email));
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        const id = doc.id
+        datas.push({
+          id,
+          ...doc.data()
+        })
+      });
+
+      if (datas.length < 1) {
+        throw new Error("Email is incorrect. Please try again")
+      }
+      const data = datas[0];
+      const isValidatePassword = await bcrypt.compareSync(password, data.password)
+      if (!isValidatePassword) {
+        throw new Error("Password incorrect. Please try again");
+      }
+      let payload = {
+        userId: data.id
+      }
+      let token = jwt.sign(payload, 'secret', { expiresIn: "10h" });
+
+      res.json({ login: "success", token })
+
+    } catch (error) {
       next(error)
     }  
   })
